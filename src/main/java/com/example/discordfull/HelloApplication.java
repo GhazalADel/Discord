9dce69889dc9d94af01ded3eb63d7a72b5363948
Ghazal <gashoftedel@gmail.com>
2022-07-03 10:40:04 +0430

    some elements in profile created!

--- src/main/java/com/example/discordfull/HelloApplication.java
+++ src/main/java/com/example/discordfull/HelloApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("profile-settings.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello world");
         stage.setScene(scene);
         stage.show();
