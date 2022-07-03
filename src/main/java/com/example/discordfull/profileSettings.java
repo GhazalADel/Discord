021fe42ea89afb4dfb4fa7b109699f22cfef24ea
Ghazal <gashoftedel@gmail.com>
2022-07-03 12:09:39 +0430

    profile UI completed.

--- src/main/java/com/example/discordfull/profileSettings.java
+++ src/main/java/com/example/discordfull/profileSettings.java
@@ -1,4 +1,15 @@
 package com.example.discordfull;
 
-public class profileSettings {
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class profileSettings implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+    }
 }
