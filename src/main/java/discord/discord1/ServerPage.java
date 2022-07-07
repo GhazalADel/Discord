48638445a0cec82bd64fe8f5f0f72957cba96551
Ghazal <gashoftedel@gmail.com>
2022-07-07 21:22:40 +0430

    profile settings in server page completed.

--- src/main/java/discord/discord1/ServerPage.java
+++ src/main/java/discord/discord1/ServerPage.java
@@ -1,4 +1,27 @@
 package discord.discord1;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ServerPage {
+    @FXML
+    private Circle statusCircle;
+    @FXML
+    void settingClick(MouseEvent event) {
+        Stage stage= (Stage) statusCircle.getScene().getWindow();
+        Parent root= null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("profile-settings.fxml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.setScene(new Scene(root));
+    }
 }
