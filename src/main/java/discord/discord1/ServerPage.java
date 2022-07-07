6f859a043a6a3d9c1fec91ef11716352c02143de
Ghazal <gashoftedel@gmail.com>
2022-07-07 21:27:38 +0430

    status circle in server page completed.

--- src/main/java/discord/discord1/ServerPage.java
+++ src/main/java/discord/discord1/ServerPage.java
@@ -2,17 +2,24 @@ package discord.discord1;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ServerPage {
+public class ServerPage implements Initializable {
     @FXML
     private Circle statusCircle;
+
+    @FXML
+    private Circle profileCircle;
     @FXML
     void settingClick(MouseEvent event) {
         Stage stage= (Stage) statusCircle.getScene().getWindow();
@@ -23,5 +30,33 @@ public class ServerPage {
             throw new RuntimeException(e);
         }
         stage.setScene(new Scene(root));
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        UIRequest uiRequest=new UIRequest(UIRequestCode.GET_STATUS);
+        UIResponse uiResponse;
+        try {
+            uiResponse=Client.process(uiRequest);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        Status userStatus= (Status) uiResponse.getData("status");
+        if(userStatus==Status.ONLINE){
+            statusCircle.setFill(Paint.valueOf("#3ba55d"));
+        }
+        else if(userStatus==Status.DO_NOT_DISTURB){
+            statusCircle.setFill(Paint.valueOf("#e03f41"));
+        }
+        else if(userStatus==Status.IDLE){
+            statusCircle.setFill(Paint.valueOf("#eb9e19"));
+        }
+        else if(userStatus==Status.INVISIBLE){
+            statusCircle.setFill(Paint.valueOf("##747f8d"));
+        }
+
+
     }
 }
